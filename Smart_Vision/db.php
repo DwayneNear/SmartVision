83b01fd63b049f2f5dac4f788c775b17bf90b654
DwayneNear <19179810067zz@gmail.com>
2025-05-18 20:03:26 +0800

    Delete Smart_Vision directory

--- Smart_Vision/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Connect to the MySQL database
-$conn = mysqli_connect("localhost", "root", "", "examp");
-
-// Check for connection errors
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-?>
